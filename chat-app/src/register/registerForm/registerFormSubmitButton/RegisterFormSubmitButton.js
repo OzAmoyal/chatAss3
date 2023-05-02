4752664cae0d3451968cf2267eb65c9706315476
topaz <topazavraham9@gmail.com>
2023-05-02 21:06:27 +0300

    final final commit

--- chat-app/src/register/registerForm/registerFormSubmitButton/RegisterFormSubmitButton.js
+++ chat-app/src/register/registerForm/registerFormSubmitButton/RegisterFormSubmitButton.js
@@ -1,27 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 function RegisterFormSubmitButton({ formData, registered, setRegisteredUsers}) {
     const enableButton = (formData) =>Object.values(formData["allowedSubmit"]).some((value) => value === false);
     const isButtonDisabled = enableButton(formData);
-    const navigate = useNavigate();
-    const handleSubmit = (event) => {
-        if(formData["allowedSubmit"]["username"] && formData["allowedSubmit"]["password"] && 
-            formData["allowedSubmit"]["confirm"] && formData["allowedSubmit"]["display"] &&
-            formData["allowedSubmit"]["picture"]){
-        
-            setRegisteredUsers(prevState => [...prevState, {username: formData["username"], password: formData["password"], picture: formData["picture"], display: formData["display"], users: []}]);        
-            navigate('/login');
-        }
-        else{
-            event.preventDefault();
-        }
-    }
+
     return (
         <button
             type="submit"
             className="btn btn-info text-white"
-            onClick={(event) => handleSubmit(event)}
             disabled={isButtonDisabled}
         >
             Submit
